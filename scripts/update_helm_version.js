fd7b53e7c8ab20671d79f03ce72cbba7c5bfb722
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-02-12 13:44:36 -0800

    Adding single_node_install.sh script to version update script

--- scripts/update_helm_version.js
+++ scripts/update_helm_version.js
@@ -12,11 +12,18 @@ const values = yaml.load(fs.readFileSync(valuesPath, "utf8"));
 values.service.image.tag = version;
 fs.writeFileSync(valuesPath, yaml.dump(values), "utf8");
 
+// Update the version in the single_node_install.sh script
+const scriptRegex = /VERSION="\b\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?\b"/g;
+const scriptPath = path.join(__dirname, "..", "single_node_install.sh");
+const script = fs.readFileSync(scriptPath, { encoding: "utf-8" });
+let updated = script.replace(scriptRegex, version);
+fs.writeFileSync(scriptPath, updated, { encoding: "utf-8" });
+
 // Also update the example in the README with the correct tag
-const versionRegex = /\b\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?\b/g;
+const readmeRegex = /\b\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?\b/g;
 const readmePath = path.join(__dirname, "..", "README.md");
 const readme = fs.readFileSync(readmePath, { encoding: "utf-8" });
-const updated = readme.replace(versionRegex, version);
+updated = readme.replace(readmeRegex, version);
 fs.writeFileSync(readmePath, updated, { encoding: "utf-8" });
 
 console.log(`Updated Helm values.yaml to version ${version}`);
