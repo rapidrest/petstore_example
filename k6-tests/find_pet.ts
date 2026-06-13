fe453bc5dd4dbfb827fe871647b570b83cab3bc1
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-06-13 10:20:46 -0700

    Adding new find_pet k6 script

---
+++ k6-tests/find_pet.ts
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 100,
+  duration: '30s'
+};
+
+export const config = {
+  url: __ENV.url || 'http://localhost:3000',
+  id: __ENV.category || "bunny"
+};
+
+export function findPets(url: string, category: string) {
+  const res = http.get(url + "/pet?category=" + category);
+  check(res, { "status is 200": (res) => res.status === 200 });
+  check(res, { "res.body is defined": (res) => res.body !== undefined });
+  if (res && res.body) {
+    return JSON.parse(res.body.toString());
+  } else {
+    throw new Error("Failed to retrieve pets with category: " + category);
+  }
+}
+
+export default function() {
+  findPets(config.url, config.id);
+}
