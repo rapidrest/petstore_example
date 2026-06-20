cdedd933e84892d35fb65c3b715157c9a0bbfe66
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-06-20 15:33:27 -0700

    Adding load test script

---
+++ scripts/run_load_test.ps1
@@ -0,0 +1,9 @@
+k6 run .\k6-tests\setup.ts
+clear
+k6 run .\k6-tests\create_pet.ts
+k6 run .\k6-tests\create_user.ts
+k6 run .\k6-tests\find_pet.ts
+k6 run .\k6-tests\get_pet.ts
+k6 run .\k6-tests\get_status.ts
+k6 run .\k6-tests\get_user.ts
+k6 run .\k6-tests\login.ts
