60c1560b56beb2051de946a2f3ccb5479fe2af3f
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-07-05 20:59:13 -0700

    Upgrading to service-core 1.0.0-rc.12 Refactoring OrderRoute, PetRoute and UserRoute to use CRUDRoute

--- src/routes/PetRoute.ts
+++ src/routes/PetRoute.ts
@@ -3,38 +3,14 @@
 ///////////////////////////////////////////////////////////////////////////////
 import {
     RouteDecorators,
-    DocDecorators,
-    ModelRoute,
-    RepoUtils,
-    UpdateObject,
-    HttpResponse,
-    HttpRequest,
-    ApiErrors,
-    ApiErrorMessages,
-    ModelUtils,
-    BaseEntity
+    CRUDRoute
 } from "@rapidrest/service-core";
 import Pet from "../models/Pet.js";
-import { ApiError, JWTUser, ObjectDecorators} from "@rapidrest/core";
 
-const { Description, Returns, Summary, TypeInfo } = DocDecorators;
-const { Init } = ObjectDecorators;
 const {
-    Auth,
-    Delete,
-    Get,
-    Head,
     Model,
-    Query,
-    Param,
-    Post,
-    Put,
-    Request,
-    Response,
     Route,
-    Validate
 } = RouteDecorators;
-const AuthUser = RouteDecorators.User;
 
 /**
  * Handles all REST API requests for the endpoint `/pet`.
@@ -43,117 +19,5 @@ const AuthUser = RouteDecorators.User;
  */
 @Model(Pet)
 @Route("/pet")
-class PetRoute extends ModelRoute<Pet> {
-    protected repoUtilsClass: any = RepoUtils;
-
-    @Summary("Count Pets")
-    @Description("Returns the total count of Pets in the datastore based on the given criteria "
-        + "in the header as `Content-Length`.")
-    @Returns([Object])
-    @Head()
-    private async count(
-        @Param() params: any,
-        @Query() query: any,
-        @Response res: HttpResponse,
-        @AuthUser user: JWTUser
-    ): Promise<any> {
-        return super.doCount({ params, query, res, user });
-    }
-
-    public async validateCreate(obj: Partial<Pet> | Partial<Pet>[], @AuthUser user: JWTUser) {
-        return super.doValidate(obj, { user });
-    }
-
-    /**
-     * Create a new Pet.
-     */
-    @Summary("Create Pet")
-    @Description("Create a new Pet.")
-    @Returns([Pet])
-    @Auth(["jwt"])
-    @Post()
-    @Validate("validateCreate")
-    private create(obj: Pet | Pet[], @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<Pet | Array<Pet>> {
-        return super.doCreate(obj, { req, user });
-    }
-
-    /**
-     * Deletes the Pet
-     */
-    @Summary("Delete pet by ID")
-    @Description("Deletes the pet from the service.")
-    @Returns([null])
-    @Auth(["jwt"])
-    @Delete("/:id")
-    private async delete(@Param("id") id: string, @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<void> {
-        return super.doDelete(id, { user, req });
-    }
-
-    /**
-     * Returns all Pets from the system that the user has access to
-     */
-    @Summary("Find All Pets")
-    @Description("Returns all Pets from the system that the user has access to.")
-    @Returns([[Array, Pet]])
-    @Get()
-    private async findAll(@Param() params: any, @Query() query: any, @AuthUser user: JWTUser): Promise<Array<Pet>> {
-        return super.doFindAll({ params, query, user });
-    }
-
-    /**
-     * Returns a single Pet from the system that the user has access to
-     */
-    @Summary("Find pet by ID")
-    @Description("Returns a single Pet from the system that the user has access to.")
-    @Returns([Pet])
-    @Get("/:id")
-    private async findById(@Param("id") id: string, @Query() query: any, @AuthUser user: JWTUser): Promise<Pet | null> {
-        return super.doFindById(id, { query, user });
-    }
-
-    @Summary("Truncate Pets")
-    @Description("Deletes all Pets from the datastore that the user has access to.")
-    @Returns([null])
-    @Auth(["jwt"])
-    @Delete()
-    public async truncate(
-        @Param() params: any,
-        @Query() query: any,
-        @AuthUser user: JWTUser
-    ): Promise<void> {
-        return super.doTruncate({ params, query, user });
-    }
-
-    public async validateUpdate(@Param("id") id: string, obj: UpdateObject<Pet>, @AuthUser user: JWTUser) {
-        return super.doValidate(obj, { user });
-    }
-
-    /**
-     * Updates a single Pet
-     */
-    @Summary("Update pet by ID")
-    @Description("Updates a single Pet.")
-    @Returns([Pet])
-    @Auth(["jwt"])
-    @Put("/:id")
-    @Validate("validateUpdate")
-    private async update(@Param("id") id: string, obj: UpdateObject<Pet>, @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<Pet> {
-        return super.doUpdate(id, obj, { user });
-    }
-
-    @Summary("Update pet by ID and property")
-    @Put(":id/:property")
-    @Description("Updates a single property of an existing pet.")
-    @TypeInfo([Object])
-    @Returns([Pet])
-    protected updateProperty(
-        @Param("id") id: string,
-        @Param("property") propertyName: string,
-        obj: any,
-        @AuthUser user: JWTUser
-    ): Promise<Pet> {
-        return super.doUpdateProperty(id, propertyName, obj, { user });
-    }
+export default class PetRoute extends CRUDRoute<Pet> {
 }
-
-export default PetRoute;
--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -4,33 +4,17 @@
 import * as argon from "argon2";
 import {
     RouteDecorators,
-    DocDecorators,
-    ModelRoute,
-    RepoUtils,
     UpdateObject,
     ApiErrorMessages,
-    HttpResponse,
-    HttpRequest
+    CRUDRoute
 } from "@rapidrest/service-core";
 import User from "../models/User.js";
-import { ApiError, JWTUser, ObjectDecorators, UserUtils} from "@rapidrest/core";
+import { ApiError, JWTUser, UserUtils} from "@rapidrest/core";
 
-const { Description, Returns, Summary, TypeInfo } = DocDecorators;
-const { Config } = ObjectDecorators;
 const {
-    Auth,
-    Delete,
-    Get,
-    Head,
     Model,
-    Query,
     Param,
-    Post,
-    Put,
-    Request,
-    Response,
     Route,
-    Validate
 } = RouteDecorators;
 const AuthUser = RouteDecorators.User;
 
@@ -41,29 +25,9 @@ const AuthUser = RouteDecorators.User;
  */
 @Model(User)
 @Route("/user")
-class UserRoute extends ModelRoute<User> {
-    protected repoUtilsClass: any = RepoUtils;
-
-    @Config("trusted_roles", ["admin"])
-    protected trustedRoles: string[] = [];
-
-    @Summary("Count Users")
-    @Description("Returns the total count of Users in the datastore based on the given criteria "
-        + "in the header as `Content-Length`.")
-    @Returns([Object])
-    @Auth(["jwt"])
-    @Head()
-    private async count(
-        @Param() params: any,
-        @Query() query: any,
-        @Response res: HttpResponse,
-        @AuthUser user: JWTUser
-    ): Promise<any> {
-        return super.doCount({ params, query, res, user });
-    }
-
-    public async validateCreate(obj: Partial<User> | Partial<User>[], @AuthUser user: JWTUser) {
-        await super.doValidate(obj, { user });
+class UserRoute extends CRUDRoute<User> {
+    protected async validateCreate(obj: Partial<User> | Partial<User>[], @AuthUser user: JWTUser) {
+        await super.validateCreate(obj, user);
 
         obj = Array.isArray(obj) ? obj : [obj];
         for (const user of obj) {
@@ -73,69 +37,8 @@ class UserRoute extends ModelRoute<User> {
         }
     }
 
-    /**
-     * Create a new User.
-     */
-    @Summary("Create User")
-    @Description("Create a new User.")
-    @Returns([User])
-    @Post()
-    @Validate("validateCreate")
-    private async create(obj: User | User[], @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<User | Array<User>> {
-        return super.doCreate(obj, { user, req });
-    }
-
-    /**
-     * Deletes the User
-     */
-    @Summary("Delete user by ID")
-    @Description("Deletes the user from the service.")
-    @Returns([null])
-    @Auth(["jwt"])
-    @Delete("/:id")
-    private async delete(@Param("id") id: string, @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<void> {
-        return super.doDelete(id, { user, req });
-    }
-
-    /**
-     * Returns all Users from the system that the user has access to
-     */
-    @Summary("Find All Users")
-    @Description("Returns all Users from the system that the user has access to.")
-    @Returns([[Array, User]])
-    @Auth(["jwt"])
-    @Get()
-    private async findAll(@Param() params: any, @Query() query: any, @AuthUser user: JWTUser): Promise<Array<User>> {
-        return super.doFindAll({ params, query, user });
-    }
-
-    /**
-     * Returns a single User from the system that the user has access to
-     */
-    @Summary("Find user by ID")
-    @Description("Returns a single User from the system that the user has access to.")
-    @Returns([User])
-    @Auth(["jwt"])
-    @Get("/:id")
-    private async findById(@Param("id") id: string, @Query() query: any, @AuthUser user: JWTUser): Promise<User | null> {
-        return super.doFindById(id, { query, user });
-    }
-
-    @Summary("Truncate Users")
-    @Description("Deletes all Users from the datastore that the user has access to.")
-    @Returns([null])
-    @Auth(["jwt"])
-    @Delete()
-    public async truncate(
-        @Param() params: any,
-        @Query() query: any,
-        @AuthUser user: JWTUser
-    ): Promise<void> {
-        return super.doTruncate({ params, query, user });
-    }
-
     public async validateUpdate(@Param("id") id: string, obj: UpdateObject<User>, @AuthUser user: JWTUser) {
-        await super.doValidate(obj, { user });
+        await super.validateUpdate(id, obj, user);
 
         // Only admins and the user itself can make changes
         if (!UserUtils.hasRoles(user, this.trustedRoles) && (id !== user.uid || obj.uid !== user.uid)) {
@@ -145,38 +48,6 @@ class UserRoute extends ModelRoute<User> {
         if (obj.password) {
             obj.password = await argon.hash(obj.password);
         }
-    }
-
-    /**
-     * Updates a single User
-     */
-    @Summary("Update user by ID")
-    @Description("Updates a single User.")
-    @Returns([User])
-    @Auth(["jwt"])
-    @Put("/:id")
-    @Validate("validateUpdate")
-    private async update(@Param("id") id: string, obj: UpdateObject<User>, @Request req: HttpRequest, @AuthUser user: JWTUser): Promise<User> {
-        return super.doUpdate(id, obj, { user });
-    }
-
-    @Summary("Update user by ID and property")
-    @Put(":id/:property")
-    @Description("Updates a single property of an existing user.")
-    @TypeInfo([Object])
-    @Returns([User])
-    protected updateProperty(
-        @Param("id") id: string,
-        @Param("property") propertyName: string,
-        obj: any,
-        @AuthUser user: JWTUser
-    ): Promise<User> {
-        // Only admins and the user itself can make changes
-        if (!UserUtils.hasRoles(user, this.trustedRoles) && (id !== user.uid || obj.uid !== user.uid)) {
-            throw new ApiError(ApiErrorMessages.AUTH_PERMISSION_FAILURE, 403, ApiErrorMessages.AUTH_PERMISSION_FAILURE);
-        }
-
-        return super.doUpdateProperty(id, propertyName, obj, { user });
     }
 }
 
