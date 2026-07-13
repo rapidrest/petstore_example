81c4b2edd1acfa6567115fc7640ec917330c0439
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-07-13 13:32:47 -0700

    Adding two new k6 tests for React pages

---
+++ k6-tests/get_www_index.ts
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 200,
+  duration: '60s'
+};
+
+export const config = {
+  url: __ENV.url || 'http://localhost:3000'
+};
+
+export default function() {
+  const res = http.get(config.url);
+  check(res, { "status is 200": (res) => res.status === 200 });
+}
